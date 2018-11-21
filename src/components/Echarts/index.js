0d7cfee06206ab9fb0d5dbb2c1d9b7d1fd715904
LinZhiyu <linzhiyu@ifastfinancial.com.cn>
2018-11-21 17:15:15 +0800

    修改echarts的资源路径

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -22,6 +22,10 @@ export default class App extends Component {
   }
 
   render() {
+    const tplSource =
+    Platform.OS === 'ios'
+      ? require('./tpl.html')
+      : { uri: 'file:///android_asset/tpl.html' };
     return (
       <View style={{flex: 1, height: this.props.height || 400,}}>
         <WebView
@@ -33,7 +37,7 @@ export default class App extends Component {
             backgroundColor: this.props.backgroundColor || 'transparent'
           }}
           scalesPageToFit={Platform.OS !== 'ios'}
-          source={require('./tpl.html')}
+          source={tplSource}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
